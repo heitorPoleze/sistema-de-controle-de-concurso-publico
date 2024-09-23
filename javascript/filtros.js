a75a6c3e1b4bbdbbea118d21cb331c0c65c3a98f
davyods <davia.ods.7@gmail.com>
2024-09-23 20:49:22 -0300

    Ultima atualização de filtros

--- javascript/filtros.js
+++ javascript/filtros.js
@@ -15,15 +15,24 @@ inFiltro.addEventListener("input", function(){
 function filtroNome(){
     let nome = document.getElementById("inFiltro").value;
     let nomeFiltrado = [];
+    let CPFFiltrado = [];
+    let  nascimentoFiltrado = [];
+    let pontuacaoFiltrado = [];
+    let aprovadoFiltrado = [];
+
     for(let i = 0; i < candidados.length; i++){
         if(candidados[i].toLowerCase().includes(nome.toLowerCase())){
             nomeFiltrado.push(candidados[i]);
+            CPFFiltrado.push(CPF[i]);
+            nascimentoFiltrado.push(nascimento[i]);
+            pontuacaoFiltrado.push(pontuacao[i]);
+            aprovadoFiltrado.push(aprovado[i]);
         }
     }
     listaCandidatos.innerHTML = "";
     for(let i = 0; i < nomeFiltrado.length; i++){
-        let cadaCandidato = document.createElement('li');
-        cadaCandidato.innerHTML = `Candidato: ${nomeFiltrado[i]} CPF: ${CPF[i]} Data de Nascimento:  ${nascimento[i]} Pontuação: ${pontuacao[i]} Aprovação: ${aprovado[i]}`;
+        let cadaCandidato = document.createElement('table');
+        cadaCandidato.innerHTML = `Candidato: ${nomeFiltrado[i]} CPF: ${CPFFiltrado[i]} Data de Nascimento:  ${nascimentoFiltrado[i]} Pontuação: ${pontuacaoFiltrado[i]} Aprovação: ${aprovadoFiltrado[i]}`;
         listaCandidatos.appendChild(cadaCandidato);
     }
 }
@@ -32,16 +41,26 @@ function filtroNome(){
 function filtroDtNasc(){
     let dtNascimento = document.getElementById("inFiltro").value;
     let dtNascimentoFiltrado = [];
+    let CPFFiltrado = [];
+    let pontuacaoFiltrado = [];
+    let aprovadoFiltrado = [];
+    let nomeFiltrado = [];
     for(let i = 0; i < nascimento.length; i++){
-            if(nascimento[i].toLowerCase().includes(dtNascimento.toLowerCase())){
+            if(nascimento[i].toLowerCase().includes(dtNascimento.toLowerCase())){                
                 dtNascimentoFiltrado.push(nascimento[i]);
+                CPFFiltrado.push(CPF[i]);
+                pontuacaoFiltrado.push(pontuacao[i]);
+                aprovadoFiltrado.push(aprovado[i]);
+                nomeFiltrado.push(candidados[i]);
             }
     }
     
     listaCandidatos.innerHTML = "";
     for(let i = 0; i < dtNascimentoFiltrado.length; i++){
-        let cadaCandidato = document.createElement('li');
-        cadaCandidato.innerHTML = `Candidato: ${candidados[i]} CPF: ${CPF[i]} Data de Nascimento:  ${dtNascimentoFiltrado[i]} Pontuação: ${pontuacao[i]} Aprovação: ${aprovado[i]}`;
+        let cadaCandidato = document.createElement('table');
+        cadaCandidato.innerHTML = `Candidato: ${nomeFiltrado[i]} CPF: ${CPFFiltrado[i]} Data de Nascimento:  ${dtNascimentoFiltrado[i]} Pontuação: ${pontuacaoFiltrado[i]} Aprovação: ${aprovadoFiltrado[i]}`;
         listaCandidatos.appendChild(cadaCandidato);
     }
 }
+
+
